71c927614a837c7d3f47537a6b6674a6541bf671
Jeetgour01 <erjeetgour@gmail.com>
2022-12-06 02:53:32 +0530

    added: week days

---
+++ week_names.sh
@@ -0,0 +1,16 @@
+read -p " Enter Number To Get WeekDay Name: " num
+case $num in
+1) echo "Sunday"
+;;
+2) echo "Monday"
+;;
+3) echo  "Tuesday"
+;;
+4) echo "Wednesday"
+;;
+5) echo  "Thursday"
+;;
+6) echo  "Friday"
+;;
+*) echo "Saturday"
+esac
