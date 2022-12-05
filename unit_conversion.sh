3ec4dd6b166f4deae0cbf1b15459c20ce5f2ca5d
Jeetgour01 <erjeetgour@gmail.com>
2022-12-06 01:23:23 +0530

    added: unit convertion

---
+++ unit_conversion.sh
@@ -0,0 +1,9 @@
+echo "1inch =1/12 ft"
+echo "42 in =42*1/12 ft"
+echo $((42*1/12))"ft"
+echo "1feet=0.305meter"
+echo $((2400*93025/1000000)) "in meters square"
+echo " 1 acre=4040.86 square meter"
+area=$((25*223))
+echo $area"square meter"
+echo "1 acre="$((5575*404086/100)) "square meter"
