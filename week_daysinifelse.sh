a930b3bfb14a16fcf676937a12cba3e24a90efd3
Jeetgour01 <erjeetgour@gmail.com>
2022-12-06 09:52:48 +0530

    added: week days

---
+++ week_daysinifelse.sh
@@ -0,0 +1,22 @@
+read -p " Enter week day name: " num
+if((num==0))
+then
+echo "sunday"
+elif((num==1))
+then
+echo "monday"
+elif((num==2))
+then
+echo  "tuesday"
+elif((num==3))
+then
+echo "wednesday"
+elif((num==4))
+then
+echo "thrusday"
+elif((num==5))
+then
+echo  "friday"
+else
+echo "saturday"
+fi
