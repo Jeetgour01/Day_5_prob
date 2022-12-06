99a544ca4410cfbcedf9d96c51c8415b5775cfc9
Jeetgour01 <erjeetgour@gmail.com>
2022-12-06 09:43:01 +0530

    added: single digit in words

---
+++ single_digit_words.sh
@@ -0,0 +1,31 @@
+read -p " Enter any Single Digit Number To Get It In Words: " num
+if((num==0))
+then
+echo "zero"
+elif((num==1))
+then
+echo "one"
+elif((num==2))
+then
+echo  "Two"
+elif((num==3))
+then
+echo "three"
+elif((num==4))
+then
+echo "Four"
+elif((num==5))
+then
+echo  "Five"
+elif((num==6))
+then
+echo  "Six"
+elif((num==7))
+then
+echo "seven"
+elif((num==8))
+then
+echo "eight"
+else
+echo "nine"
+fi
